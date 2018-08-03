f1c71a19a782d486f9b347c4a71d21f184ae4bf5
Vishnu KS <ksvishnu56@gmail.com>
2018-08-03 09:56:22 +0000

    updated

--- untagged_ec2.py
+++ untagged_ec2.py
@@ -1,11 +1,12 @@
 import boto3
 
 instances = [i for i in boto3.resource('ec2', region_name='us-east-1').instances.all()]
-key1 = 'sample'
-# Print instance_id of instances that do not have a Tag of Key=sample
+keys = ['key1','key2','key3']
+# replace elements in keys with your keys
 for i in instances:
   if i.tags:
-    if key1 not in [t['Key'] for t in i.tags]:
-      print i.instance_id
+    for key in keys:
+      if key not in [t['Key'] for t in i.tags]:
+        print i.instance_id+": "+key
   else:
     print("No tags found for: "+i.instance_id)
